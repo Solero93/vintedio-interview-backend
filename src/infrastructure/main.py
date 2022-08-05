04d934d19addbd63ff8548548686b4bc844d1acb
Solero93 <solero93@gmail.com>
2022-08-05 13:01:59 +0200

    Added printing of LIST command and listing packages

--- src/infrastructure/main.py
+++ src/infrastructure/main.py
@@ -1,9 +1,16 @@
+import sys
+
 from src.application.list_packages import ListPackages
 
 
 def main():
-    list_packages = ListPackages()
-    list_packages.invoke()
+    current_line = sys.stdin.readline()
+    while current_line != "END":
+        if current_line == "LIST":
+            print("LIST")
+            list_packages = ListPackages()
+            list_packages.invoke()
+
 
 
 if __name__ == "__main__":
